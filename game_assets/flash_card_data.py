4ec8a34262ed15af24ab287192fd7b0646a67e37
Joshua Reed <joshualreed93@gmail.com>
2020-03-19 15:47:56 -0400

    update true false logic

--- game_assets/flash_card_data.py
+++ game_assets/flash_card_data.py
@@ -12,33 +12,46 @@ class BaseModel(Model):
 class Card(BaseModel):
     front = CharField()
     back = CharField()
+    wrong = IntegerField()
 
 
-db.connect()
-db.drop_tables([Card])
-db.create_tables([Card])
+# db.connect()
+# db.drop_tables([Card])
+# db.create_tables([Card])
 
-Card(front='Which fictional city is the home of Batman?', back='gotham').save()
-Card(front='What is a Geiger Counter used to detect?', back='radiation').save()
-Card(front='In the film Babe, what type of animal was Babe?', back='pig').save()
-Card(front='What’s the total number of dots on a pair of dice?', back='42').save()
-Card(front='Traditionally, how many Wonders of the World are there?', back='7').save()
-Card(front='Which planet is the closest to Earth', back='venus').save()
-Card(front='According to the old proverb, to which European capital city do all roads lead?', back='rome').save()
-Card(front='Which is the tallest mammal?', back='giraffe').save()
-Card(front='What is the name of the fairy in Peter Pan?', back='tinkerbell').save()
-Card(front='How many strings does a violin have?', back='4').save()
-Card(front='The hardest natural substance known is what?', back='diamond').save()
-Card(front='In Greek mythology, who turned all that he touched into gold?',
-     back='midas').save()
-Card(front='How many sides does an octagon have?', back='8').save()
-Card(front='What is the name of the city where the cartoon family The Simpsons live?',
-     back='springfield').save()
-Card(front='The hard white material of elephant tusks is called what?',
-     back='ivory').save()
-Card(front='Which atmospheric gas is the most common?', back='nitrogen').save()
-Card(front='What kind of bird is often seen dashing along the roads and highways of the southern USA and Mexico... hence its name? (one word)', back='roadrunner').save()
-Card(front='Immersing food in vinegar to prolong its lifespan is known as what?',
-     back='pickling').save()
-Card(front='A "Grand Slam" in tennis means a player winning how many major tournaments in a calendar year?', back='4').save()
-Card(front='In what mountain range is Mount Everest located?', back='himalayas').save()
+# Card(front='Which fictional city is the home of Batman?',
+#      back='gotham', wrong=0).save()
+# Card(front='What is a Geiger Counter used to detect?',
+#      back='radiation', wrong=0).save()
+# Card(front='In the film Babe, what type of animal was Babe?',
+#      back='pig', wrong=0).save()
+# Card(front='What’s the total number of dots on a pair of dice?',
+#      back='42', wrong=0).save()
+# Card(front='Traditionally, how many Wonders of the World are there?',
+#      back='7', wrong=0).save()
+# Card(front='Which planet is the closest to Earth', back='venus', wrong=0).save()
+# Card(front='According to the old proverb, to which European capital city do all roads lead?',
+#      back='rome', wrong=0).save()
+# Card(front='Which is the tallest mammal?', back='giraffe', wrong=0).save()
+# Card(front='What is the name of the fairy in Peter Pan?',
+#      back='tinkerbell', wrong=0).save()
+# Card(front='How many strings does a violin have?', back='4', wrong=0).save()
+# Card(front='The hardest natural substance known is what?',
+#      back='diamond', wrong=0).save()
+# Card(front='In Greek mythology, who turned all that he touched into gold?',
+#      back='midas', wrong=0).save()
+# Card(front='How many sides does an octagon have?', back='8', wrong=0).save()
+# Card(front='What is the name of the city where the cartoon family The Simpsons live?',
+#      back='springfield', wrong=0).save()
+# Card(front='The hard white material of elephant tusks is called what?',
+#      back='ivory', wrong=0).save()
+# Card(front='Which atmospheric gas is the most common?',
+#      back='nitrogen', wrong=0).save()
+# Card(front='What kind of bird is often seen dashing along the roads and highways of the southern USA and Mexico... hence its name? (one word)',
+#      back='roadrunner', wrong=0).save()
+# Card(front='Immersing food in vinegar to prolong its lifespan is known as what?',
+#      back='pickling', wrong=0).save()
+# Card(front='A "Grand Slam" in tennis means a player winning how many major tournaments in a calendar year?',
+#      back='4', wrong=0).save()
+# Card(front='In what mountain range is Mount Everest located?',
+#      back='himalayas', wrong=0).save()
--- game_assets/flash_card_game.py
+++ game_assets/flash_card_game.py
@@ -6,17 +6,36 @@ db = PostgresqlDatabase('cards', user='postgres',
                         password='', host='localhost', port=5432)
 
 
-card = Card.select()
+card = list(Card.select())
+random.shuffle(card)
+counter = 0
+playing = None
+print("Hello welcome to trivia night, have fun and good luck! \n")
+# input("would you like to play the game or create some trivia cards of your own? enter 'game' or 'create' \n")
 
 
-def game(card):
-    for Card in list(card):
-        print(Card.front)
-        choice = input(" \nTake your best guess \n")
-        if choice == Card.back:
-            print('hey')
-        else:
-            print('ho')
+def game(card, counter):
+    print(f"\n{card[counter].front}")
+    choice = input(" \nTake your best guess \n")
+    if choice == card[counter].back:
+        playing = True
+        counter += 1
+        print('hey')
+        return playing
+    else:
+        playing = False
+        print('ho')
+        card[counter].wrong += 1
+        update = Card.update(
+            wrong=card[counter].wrong).where(Card.front == card[counter].front)
+
+        update.execute()
+        return playing
 
 
-game(card)
+print('Prompt 1')
+game(card, counter)
+if playing == True:
+    print('boo')
+else:
+    print("goodjob")
